0694df040e2b3ece3322e3dfa926e275d42f912c
zhuganglie <pyrrhonianpig@gmail.com>
2024-09-19 01:55:53 +0800

    feat: add post fetching functionality

---
+++ src/lib/posts.js
@@ -0,0 +1,75 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+const postsDirectory = path.join(process.cwd(), 'src/posts')
+
+export function getSortedPostsData() {
+  // Get file names under /src/posts
+  const fileNames = fs.readdirSync(postsDirectory)
+    .filter(file => file.endsWith('.md'))
+
+  // Remove '.md' from file names
+  const allPostsData = fileNames.map(fileName => {
+    const id = fileName.replace(/\\\\.md$/, '')
+
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+    // Separate title and date
+    const matterResult = matter(fileContents)
+
+    return {
+      id,
+      ...matterResult.data
+    }
+  })
+
+  // Sort posts by date
+  return allPostsData.sort(({ date: a }, { date: b }) => {
+    if (a < b) {
+      return 1
+    } else if (a > b) {
+      return -1
+    } else {
+      return 0
+    }
+  })
+}
+
+
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory)
+  return fileNames.map(fileName => {
+    return {
+      params: {
+        slug: fileName.replace(/\\\\.md$/, '')
+      }
+    }
+  })
+}
+
+export function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`)
+  const fileContents = fs.readFileSync(fullPath, 'utf8')
+
+  // Use gray-matter to parse the post metadata
+  const matterResult = matter(fileContents)
+
+  // Combine the data with the id
+  return {
+    id,
+    ...matterResult.data
+  }
+}
+
+export function getPosts() {
+  const allPostsData = getSortedPostsData()
+  return allPostsData.map((post) => ({
+    slug: post.id,
+    title: post.title,
+    date: post.date
+  }))
+}
