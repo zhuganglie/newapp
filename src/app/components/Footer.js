ad9adb7f1b2e49188e2b6bc2d8f8600a60dd33aa
zhuganglie <pyrrhonianpig@gmail.com>
2024-09-18 23:35:46 +0800

    feat: Add navigation and footer components

---
+++ src/app/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 My Blog</p>
+    </footer>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
-import './globals.css'
+import './globals.css';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 export default function RootLayout({ children }) {
   return (
@@ -8,7 +10,11 @@ export default function RootLayout({ children }) {
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Navigation />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
