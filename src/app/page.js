8b487ec4fa6b651b739b3bbed6f6974ae3bb66ba
zhuganglie <pyrrhonianpig@gmail.com>
2024-09-21 00:14:50 +0800

    feat: Fetch and display posts on the homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await getPosts()
   return (
     <div>
       <h1>Welcome to My Blog</h1>
