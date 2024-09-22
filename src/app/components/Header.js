674d89bbe972ca5190dd7b888bc750b07ca9ba77
zhuganglie <pyrrhonianpig@gmail.com>
2024-09-22 21:58:10 +0800

    refactor: Remove unused Breadcrumbs component from Header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,12 +1,13 @@
 "use client"
-import Breadcrumbs from './Breadcrumbs';
+
+
 
 export default function Header() {
-  const pathname = usePathname();
+
   return (
     <header className="bg-gray-900">
       <nav className="container mx-auto px-4 py-6">
-        <Breadcrumbs />
+
       </nav>
     </header>
   );
